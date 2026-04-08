5cf007524589b2f7d5636e7c39e261e22a4bfe07
prettysorrow <artyombaly@list.ru>
2026-04-08 23:26:12 +0300

    promote old tests
    Signed-off-by: prettysorrow <artyombaly@list.ru>

--- Convolution.Tests/Parallel.cs
+++ Convolution.Tests/Parallel.cs
@@ -21,18 +21,6 @@ public class Parallel
     }
 
     [Fact]
-    public void Apply_Blur3_ProducesDifferentImage()
-    {
-        using var input = imageGenerator.Generate();
-        using var result = Impl.Parallel.Apply(input, Filter.Blur3);
-
-        Assert.Equal(input.Width, result.Width);
-        Assert.Equal(input.Height, result.Height);
-
-        Assert.False(input.Equal(result));
-    }
-
-    [Fact]
     public void Apply_Sharpen_ProducesDifferentImage()
     {
         using var input = imageGenerator.Generate();
@@ -40,6 +28,18 @@ public class Parallel
 
         Assert.Equal(input.Width, result.Width);
         Assert.Equal(input.Height, result.Height);
+        Assert.False(input.Equal(result));
+    }
+
+    [Fact]
+    public void Apply_BoxBlur_ProducesDifferentImage()
+    {
+        using var input = imageGenerator.Generate();
+        using var result = Impl.Parallel.Apply(input, Filter.BoxBlur(radius: 1));
+
+        Assert.Equal(input.Width, result.Width);
+        Assert.Equal(input.Height, result.Height);
+
         Assert.False(input.Equal(result));
     }
 }
