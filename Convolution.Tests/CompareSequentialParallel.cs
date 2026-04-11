d75a97498229dea8a8fce334a2b5c7f6a46871da
prettysorrow <artyombaly@list.ru>
2026-04-11 04:06:17 +0300

    add more tests for compare parallel and sequential implementations
    Signed-off-by: prettysorrow <artyombaly@list.ru>

--- Convolution.Tests/CompareSequentialParallel.cs
+++ Convolution.Tests/CompareSequentialParallel.cs
@@ -2,54 +2,84 @@ namespace Convolution.Tests;
 
 using Convolution.Core;
 using Convolution.Extensions;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 using Xunit;
 
 public class CompareSequentialParallel
 {
     private static readonly ImageGenerator imageGenerator = new();
+    private static readonly FilterGenerator filterGenerator = new();
 
-    [Fact]
-    public void Apply_IdentityFilter()
+    private static void RunSingle(Image<Rgb24> image, Filter filter)
     {
-        using var input = imageGenerator.Generate();
-
-        using var parallelResult = Impl.Parallel.Apply(input, Filter.Identity);
-        using var sequentialResult = Impl.Sequential.Apply(input, Filter.Identity);
-
+        using var parallelResult = Impl.Parallel.Apply(image, filter);
+        using var sequentialResult = Impl.Sequential.Apply(image, filter);
         Assert.True(parallelResult.Equal(sequentialResult, tolerance: 2));
     }
 
     [Fact]
-    public void Apply_Sharpen()
+    public void Apply_IdentityFilter()
     {
-        using var input = imageGenerator.Generate();
-
-        using var parallelResult = Impl.Parallel.Apply(input, Filter.Sharpen);
-        using var sequentialResult = Impl.Sequential.Apply(input, Filter.Sharpen);
-
-        Assert.True(parallelResult.Equal(sequentialResult, tolerance: 2));
+        using var input = imageGenerator.Next();
+        var filter = Filter.Identity;
+        RunSingle(input, filter);
     }
 
     [Fact]
     public void Apply_BoxBlur()
     {
-        using var input = imageGenerator.Generate();
-
-        using var parallelResult = Impl.Parallel.Apply(input, Filter.BoxBlur(radius: 1));
-        using var sequentialResult = Impl.Sequential.Apply(input, Filter.BoxBlur(radius: 1));
-
-        Assert.True(parallelResult.Equal(sequentialResult, tolerance: 2));
+        using var input = imageGenerator.Next();
+        var filter = Filter.BoxBlur(radius: 1);
+        RunSingle(input, filter);
     }
 
 
     [Fact]
     public void Apply_GaussianBlur()
     {
-        using var input = imageGenerator.Generate();
+        using var input = imageGenerator.Next();
+        var filter = Filter.GaussianBlur(size: 3, sigma: 0.5);
+        RunSingle(input, filter);
+    }
 
-        using var parallelResult = Impl.Parallel.Apply(input, Filter.GaussianBlur(size: 3, sigma: 0.5));
-        using var sequentialResult = Impl.Sequential.Apply(input, Filter.GaussianBlur(size: 3, sigma: 0.5));
+    [Fact]
+    public void Apply_SobelX()
+    {
+        using var input = imageGenerator.Next();
+        var filter = Filter.SobelX;
+        RunSingle(input, filter);
+    }
 
-        Assert.True(parallelResult.Equal(sequentialResult, tolerance: 2));
+    [Fact]
+    public void Apply_SobelY()
+    {
+        using var input = imageGenerator.Next();
+        var filter = Filter.SobelY;
+        RunSingle(input, filter);
+    }
+
+    [Fact]
+    public void Apply_PrewittX()
+    {
+        using var input = imageGenerator.Next();
+        var filter = Filter.PrewittX;
+        RunSingle(input, filter);
+    }
+
+    [Fact]
+    public void Apply_PrewittY()
+    {
+        using var input = imageGenerator.Next();
+        var filter = Filter.PrewittY;
+        RunSingle(input, filter);
+    }
+
+    [Fact]
+    public void Apply_RandomFilter5()
+    {
+        using var input = imageGenerator.Next();
+        var filter = filterGenerator.Next(size: 5);
+        RunSingle(input, filter);
     }
 }
