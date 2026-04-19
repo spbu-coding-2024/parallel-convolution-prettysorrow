b5c24d56b23ecd9e047aa259165475ba04e87bcb
prettysorrow <artyombaly@list.ru>
2026-04-19 23:34:48 +0300

    replace Rgb24 with RgbaVector in Convolution.FilterExtensions
    Signed-off-by: prettysorrow <artyombaly@list.ru>

--- Convolution.Extensions/FilterExtensions.cs
+++ Convolution.Extensions/FilterExtensions.cs
@@ -6,34 +6,41 @@ using SixLabors.ImageSharp.PixelFormats;
 
 public static class FilterExtensions
 {
-    public static Rgb24 ApplyToRgb24(this Filter filter, Image<Rgb24> source, int x, int y)
+    public static RgbaVector FilterOnePixel(this Image<RgbaVector> source, Filter filter, int x, int y)
     {
         int kSize = filter.Kernel.GetLength(0);
         int offset = kSize / 2;
+        float r = 0, g = 0, b = 0;
 
-        double r = 0, g = 0, b = 0;
         for (int ky = 0; ky < kSize; ky++)
         {
             for (int kx = 0; kx < kSize; kx++)
             {
                 int srcX = x + kx - offset;
                 int srcY = y + ky - offset;
-                srcX = Math.Clamp(srcX, 0, source.Width - 1);
-                srcY = Math.Clamp(srcY, 0, source.Height - 1);
+
+                switch (filter.edgeMode)
+                {
+                    case Filter.EdgeMode.Clamp:
+                        srcX = Math.Clamp(srcX, 0, source.Width - 1);
+                        srcY = Math.Clamp(srcY, 0, source.Height - 1);
+                        break;
+                    case Filter.EdgeMode.Wrap:
+                        srcX = ((srcX % source.Width) + source.Width) % source.Width;
+                        srcY = ((srcY % source.Height) + source.Height) % source.Height;
+                        break;
+                }
+
                 var pixel = source[srcX, srcY];
-                double weight = filter.Kernel[ky, kx];
+                float weight = (float)filter.Kernel[ky, kx];
                 r += pixel.R * weight;
                 g += pixel.G * weight;
                 b += pixel.B * weight;
             }
         }
 
-        double factor = filter.Factor;
-        double bias = filter.Bias;
-        return new Rgb24(
-            (byte)Math.Clamp((int)((factor * r) + bias), 0, 255),
-            (byte)Math.Clamp((int)((factor * g) + bias), 0, 255),
-            (byte)Math.Clamp((int)((factor * b) + bias), 0, 255)
-        );
+        float factor = (float)filter.Factor;
+        float bias = (float)filter.Bias;
+        return new RgbaVector((factor * r) + bias, (factor * g) + bias, (factor * b) + bias);
     }
 }
