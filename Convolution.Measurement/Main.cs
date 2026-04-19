6792132e33f814b217ba131a3f557ed0be22aa65
prettysorrow <artyombaly@list.ru>
2026-04-19 23:32:58 +0300

    disable stylecop warning in Convolution.Measurements.Main
    Signed-off-by: prettysorrow <artyombaly@list.ru>

--- Convolution.Measurement/Main.cs
+++ Convolution.Measurement/Main.cs
@@ -1,6 +1,8 @@
-using Convolution.Measurement;
+#pragma warning disable SA1200 // Using directives should be placed correctly
+
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
+using Convolution.Measurement;
 
 var config = DefaultConfig.Instance
     .WithArtifactsPath("Artifacts"); // write results into "<repo root>/Artifacts"
