32b285cf0025d248409d5d0b635ada37f0b1083b
prettysorrow <artyombaly@list.ru>
2026-04-19 23:37:51 +0300

    replace Rgb24 with RgbaVector in Convolution.Impl.Parallel
    Signed-off-by: prettysorrow <artyombaly@list.ru>

--- Convolution.Impl/Parallel.cs
+++ Convolution.Impl/Parallel.cs
@@ -7,70 +7,53 @@ using SixLabors.ImageSharp.PixelFormats;
 
 public static class Parallel
 {
-    public static Image<Rgb24> Apply(Image<Rgb24> source, Filter filter)
+    public static Image<RgbaVector> Apply(Image<RgbaVector> source, Filter filter)
         => ApplyRows(source, filter);
 
-
-    public static Image<Rgb24> ApplyRows(Image<Rgb24> source, Filter filter)
+    public static Image<RgbaVector> ApplyRows(Image<RgbaVector> source, Filter filter)
     {
-        Image<Rgb24> result = new(source.Width, source.Height);
-
+        Image<RgbaVector> result = new(source.Width, source.Height);
         int batchSize = Math.Max(1, source.Height / Environment.ProcessorCount);
-        int batchesAmount = (source.Height + batchSize - 1) / batchSize;  // ceiled source.Height / batchSize
-
+        int batchesAmount = (source.Height + batchSize - 1) / batchSize; // ceiled (source.Height / batchSize)
         System.Threading.Tasks.Parallel.For(0, batchesAmount, batchIndex =>
         {
-            int startX = 0;
-            int endX = source.Width;
             int startY = batchIndex * batchSize;
             int endY = Math.Min(startY + batchSize, source.Height);
-            ApplyOneTile(source, result, filter, startY, endY, startX, endX);
+            ApplyOneTile(source, result, filter, startY, endY, 0, source.Width);
         });
-
         return result;
     }
 
-    public static Image<Rgb24> ApplyColumns(Image<Rgb24> source, Filter filter)
+    public static Image<RgbaVector> ApplyColumns(Image<RgbaVector> source, Filter filter)
     {
-        Image<Rgb24> result = new(source.Width, source.Height);
-
+        Image<RgbaVector> result = new(source.Width, source.Height);
         int batchSize = Math.Max(1, source.Width / Environment.ProcessorCount);
-        int batchesAmount = (source.Width + batchSize - 1) / batchSize;  // ceiled source.Width / batchSize
-
+        int batchesAmount = (source.Width + batchSize - 1) / batchSize; // ceiled (source.Width / batchSize)
         System.Threading.Tasks.Parallel.For(0, batchesAmount, batchIndex =>
         {
             int startX = batchIndex * batchSize;
             int endX = Math.Min(startX + batchSize, source.Width);
-            int startY = 0;
-            int endY = source.Height;
-
-            ApplyOneTile(source, result, filter, startY, source.Height, startX, endX);
+            ApplyOneTile(source, result, filter, 0, source.Height, startX, endX);
         });
-
         return result;
     }
 
-    public static Image<Rgb24> ApplyTiles(Image<Rgb24> source, Filter filter, int? tileSize)
+    public static Image<RgbaVector> ApplyTiles(Image<RgbaVector> source, Filter filter, int? tileSize)
     {
-        Image<Rgb24> result = new(source.Width, source.Height);
-
-        int _tileSize = tileSize ?? EstimateOptimalTileSize(source.Width, source.Height); // TODO: weird
-
-        int tilesAcross = (source.Width + _tileSize - 1) / _tileSize;  // ceiled source.Width / _tileSize
-        int tilesDown = (source.Height + _tileSize - 1) / _tileSize;   // ceiled source.Height / _tileSize
+        Image<RgbaVector> result = new(source.Width, source.Height);
+        int tileSize_ = tileSize ?? EstimateOptimalTileSize(source.Width, source.Height); // TODO: weird
+        int tilesAcross = (source.Width + tileSize_ - 1) / tileSize_; // ceiled (source.Width / tileSize_)
+        int tilesDown = (source.Height + tileSize_ - 1) / tileSize_;  // ceiled (source.Height / tileSize_)
         int totalTiles = tilesAcross * tilesDown;
-
         System.Threading.Tasks.Parallel.For(0, totalTiles, tileIndex =>
         {
             // left-to-right-top-to-bottom tiles traverse order
             int tileRow = tileIndex / tilesAcross;
             int tileColumn = tileIndex % tilesAcross;
-
-            int startX = tileColumn * _tileSize;
-            int endX = Math.Min(startX + _tileSize, source.Width);
-            int startY = tileRow * _tileSize;
-            int endY = Math.Min(startY + _tileSize, source.Height);
-
+            int startX = tileColumn * tileSize_;
+            int endX = Math.Min(startX + tileSize_, source.Width);
+            int startY = tileRow * tileSize_;
+            int endY = Math.Min(startY + tileSize_, source.Height);
             ApplyOneTile(source, result, filter, startY, endY, startX, endX);
         });
 
@@ -81,14 +64,13 @@ public static class Parallel
     {
         int totalPixels = width * height;
         int pixelsPerTile = totalPixels / Environment.ProcessorCount;
-        int estimate = (int)Math.Max(16, Math.Sqrt(pixelsPerTile)); // TODO: is 16 OK?
-        return estimate;
+        return (int)Math.Max(16, Math.Sqrt(pixelsPerTile)); // TODO: is 16 OK?
     }
 
     [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
     private static void ApplyOneTile(
-        Image<Rgb24> source,
-        Image<Rgb24> destination,
+        Image<RgbaVector> source,
+        Image<RgbaVector> destination,
         Filter filter,
         int startY,
         int endY,
@@ -96,7 +78,11 @@ public static class Parallel
         int endX)
     {
         for (int y = startY; y < endY; y++)
+        {
             for (int x = startX; x < endX; x++)
-                destination[x, y] = filter.ApplyToRgb24(source, x, y);
+            {
+                destination[x, y] = source.FilterOnePixel(filter, x, y);
+            }
+        }
     }
 }
