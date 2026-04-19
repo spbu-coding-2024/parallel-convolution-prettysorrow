0b9de376ca8b13bc0b352656aace6266ddc6f0ed
prettysorrow <artyombaly@list.ru>
2026-04-19 23:41:12 +0300

    fix stylecop warnings in CompareImageSharpSequential
    Signed-off-by: prettysorrow <artyombaly@list.ru>

--- Convolution.Tests/CompareImageSharpSequential.cs
+++ Convolution.Tests/CompareImageSharpSequential.cs
@@ -7,34 +7,31 @@ using Xunit;
 
 public class CompareImageSharpSequential
 {
-    private static readonly ImageGenerator imageGenerator = new();
-    private static readonly FilterGenerator filterGenerator = new();
+    private static readonly ImageGenerator ImageGenerator = new();
 
     [Fact]
     public void Apply_BoxBlur()
     {
         int radius = 1;
-        byte tolerance = 2;
 
-        using var input = imageGenerator.Next();
+        using var input = ImageGenerator.Next();
 
-        using var seqResult = Impl.Sequential.Apply(input, Filter.BoxBlur(radius));
+        using var seqResult = Impl.Sequential.Apply(input, Filters.BoxBlur(radius));
         using var sharpResult = input.Clone(ctx => ctx.BoxBlur(radius));
 
-        Assert.True(seqResult.Equal(sharpResult, tolerance));
+        Assert.True(seqResult.Equal(sharpResult));
     }
 
     [Fact]
     public void Apply_GaussianBlur()
     {
         double sigma = 1.5;
-        byte tolerance = 2;
 
-        using var input = imageGenerator.Next();
+        using var input = ImageGenerator.Next();
 
-        using var seqResult = Impl.Sequential.Apply(input, Filter.GaussianBlur(sigma));
+        using var seqResult = Impl.Sequential.Apply(input, Filters.GaussianBlur(sigma));
         using var sharpResult = input.Clone(ctx => ctx.GaussianBlur((float)sigma));
 
-        Assert.True(seqResult.Equal(sharpResult, tolerance));
+        Assert.True(seqResult.Equal(sharpResult));
     }
 }
