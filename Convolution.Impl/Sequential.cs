1ff2e2ff2797f817bad25376e5eb00b7138dcb1b
prettysorrow <artyombaly@list.ru>
2026-04-19 23:44:11 +0300

    fix warnings and replace Rgb24 with RgbaVector in Impl.Sequential
    Signed-off-by: prettysorrow <artyombaly@list.ru>

--- Convolution.Impl/Sequential.cs
+++ Convolution.Impl/Sequential.cs
@@ -7,15 +7,17 @@ using SixLabors.ImageSharp.PixelFormats;
 
 public static class Sequential
 {
-    public static Image<Rgb24> Apply(Image<Rgb24> source, Filter filter)
+    public static Image<RgbaVector> Apply(Image<RgbaVector> source, Filter filter)
     {
-        var result = new Image<Rgb24>(source.Width, source.Height);
+        var result = new Image<RgbaVector>(source.Width, source.Height);
 
-        // TODO? provide ability to change pixels processing order
         for (int y = 0; y < source.Height; y++)
+        {
             for (int x = 0; x < source.Width; x++)
-                result[x, y] = filter.ApplyToRgb24(source, x, y);
-
+            {
+                result[x, y] = source.FilterOnePixel(filter, x, y);
+            }
+        }
 
         return result;
     }
