8738c6140668b36a94f4abd2856dcd87d57823c0
prettysorrow <artyombaly@list.ru>
2026-04-29 14:52:57 +0300

    move extensions to Convolution.Core
    Signed-off-by: prettysorrow <artyombaly@list.ru>

--- Convolution.Core/ImageExtensions.cs
+++ Convolution.Core/ImageExtensions.cs
@@ -1,7 +1,6 @@
-﻿namespace Convolution.Extensions;
+﻿namespace Convolution.Core;
 
 using System;
-using Convolution.Core;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 
@@ -10,27 +9,24 @@ using SixLabors.ImageSharp.PixelFormats;
 /// </summary>
 public static class ImageExtensions
 {
-    /// <summary>
-    /// Compares two SixLabors.ImageSharp.Image(TPixel).
-    /// </summary>
-    public static bool IsEqualTo<TPixel>(this Image<TPixel> source, Image<TPixel> other, Func<TPixel, TPixel, bool> pixelComparer)
+    public static bool IsEqualTo<TPixel>(this Image<TPixel> self, Image<TPixel> other, Func<TPixel, TPixel, bool> pixelComparer)
         where TPixel : unmanaged, IPixel<TPixel>
     {
-        if (ReferenceEquals(source, other))
+        if (ReferenceEquals(self, other))
         {
             return true;
         }
 
-        if (source.Width != other.Width || source.Height != other.Height)
+        if (self.Width != other.Width || self.Height != other.Height)
         {
             return false;
         }
 
-        for (int y = 0; y < source.Height; y++)
+        for (int y = 0; y < self.Height; y++)
         {
-            for (int x = 0; x < source.Width; x++)
+            for (int x = 0; x < self.Width; x++)
             {
-                if (!pixelComparer(source[x, y], other[x, y]))
+                if (!pixelComparer(self[x, y], other[x, y]))
                 {
                     return false;
                 }
@@ -40,10 +36,7 @@ public static class ImageExtensions
         return true;
     }
 
-    /// <summary>
-    /// Compares two SixLabors.ImageSharp.Image(RgbaVector).
-    /// </summary>
-    public static bool IsEqualTo(this Image<RgbaVector> source, Image<RgbaVector> other, float tolerance = 1e-5f)
+    public static bool IsEqualTo(this Image<RgbaVector> self, Image<RgbaVector> other, float tolerance = 1e-5f)
     {
         bool Comparer(RgbaVector p1, RgbaVector p2) =>
             MathF.Abs(p1.R - p2.R) <= tolerance &&
@@ -51,18 +44,18 @@ public static class ImageExtensions
             MathF.Abs(p1.B - p2.B) <= tolerance &&
             MathF.Abs(p1.A - p2.A) <= tolerance;
 
-        return source.IsEqualTo(other, Comparer);
+        return self.IsEqualTo(other, Comparer);
     }
 
     /// <summary>
     /// Evaluates a single pixel using convolution.
     /// </summary>
     [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
-    public static RgbaVector FilterOnePixel(this Image<RgbaVector> source, Filter filter, int x, int y)
+    public static RgbaVector FilterOnePixel(this Image<RgbaVector> image, Filter filter, int x, int y)
     {
         static int Wrap(int n, int bound) => ((n % bound) + bound) % bound;
 
-        int size = filter.Kernel.GetLength(0);
+        int size = filter.KernelSize;
         int offset = size / 2;
         float r = 0, g = 0, b = 0;
 
@@ -76,16 +69,16 @@ public static class ImageExtensions
                 switch (filter.EdgeMode)
                 {
                     case EdgeMode.Clamp:
-                        srcX = Math.Clamp(srcX, 0, source.Width - 1);
-                        srcY = Math.Clamp(srcY, 0, source.Height - 1);
+                        srcX = Math.Clamp(srcX, 0, image.Width - 1);
+                        srcY = Math.Clamp(srcY, 0, image.Height - 1);
                         break;
                     case EdgeMode.Wrap:
-                        srcX = Wrap(srcX, source.Width);
-                        srcY = Wrap(srcY, source.Height);
+                        srcX = Wrap(srcX, image.Width);
+                        srcY = Wrap(srcY, image.Height);
                         break;
                 }
 
-                var pixel = source[srcX, srcY];
+                var pixel = image[srcX, srcY];
                 float weight = filter.Kernel[ky, kx];
                 r += pixel.R * weight;
                 g += pixel.G * weight;
@@ -93,8 +86,10 @@ public static class ImageExtensions
             }
         }
 
-        float factor = (float)filter.Factor;
-        float bias = (float)filter.Bias;
-        return new RgbaVector((factor * r) + bias, (factor * g) + bias, (factor * b) + bias);
+        r = (r * filter.Factor) + filter.Bias;
+        g = (g * filter.Factor) + filter.Bias;
+        b = (b * filter.Factor) + filter.Bias;
+
+        return new RgbaVector(r, g, b);
     }
 }
