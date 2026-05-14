20984773a59ddec8836376f901dc5dd9d5eb2c41
prettysorrow <prettysorrow44@gmail.com>
2026-05-14 22:14:55 +0300

    minor tests refactoring
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

--- Convolution.Tests/CompareUnsafeParallel.cs
+++ Convolution.Tests/CompareUnsafeParallel.cs
@@ -24,72 +24,72 @@ public abstract class CompareUnsafeParallel
 
     [Theory]
     [MemberData("ImageSizes")]
-    public void Apply_IdentityFilter(int width, int height)
+    public void Apply_IdentityFilter((int width, int height) imageSize)
     {
-        using var image = ImageGenerator.Next(width, height);
+        using var image = ImageGenerator.Next(imageSize.width, imageSize.height);
         var filter = Filters.Identity;
         RunSingle(image, filter);
     }
 
     [Theory]
     [MemberData("ImageSizes")]
-    public void Apply_BoxBlur(int width, int height)
+    public void Apply_BoxBlur((int width, int height) imageSize)
     {
-        using var image = ImageGenerator.Next(width, height);
+        using var image = ImageGenerator.Next(imageSize.width, imageSize.height);
         var filter = Filters.BoxBlur(radius: 1);
         RunSingle(image, filter);
     }
 
     [Theory]
     [MemberData("ImageSizes")]
-    public void Apply_GaussianBlur(int width, int height)
+    public void Apply_GaussianBlur((int width, int height) imageSize)
     {
-        using var image = ImageGenerator.Next(width, height);
+        using var image = ImageGenerator.Next(imageSize.width, imageSize.height);
         var filter = Filters.GaussianBlur(sigma: 0.5f);
         RunSingle(image, filter);
     }
 
     [Theory]
     [MemberData("ImageSizes")]
-    public void Apply_SobelX(int width, int height)
+    public void Apply_SobelX((int width, int height) imageSize)
     {
-        using var image = ImageGenerator.Next(width, height);
+        using var image = ImageGenerator.Next(imageSize.width, imageSize.height);
         var filter = Filters.SobelX;
         RunSingle(image, filter);
     }
 
     [Theory]
     [MemberData("ImageSizes")]
-    public void Apply_SobelY(int width, int height)
+    public void Apply_SobelY((int width, int height) imageSize)
     {
-        using var image = ImageGenerator.Next(width, height);
+        using var image = ImageGenerator.Next(imageSize.width, imageSize.height);
         var filter = Filters.SobelY;
         RunSingle(image, filter);
     }
 
     [Theory]
     [MemberData("ImageSizes")]
-    public void Apply_PrewittX(int width, int height)
+    public void Apply_PrewittX((int width, int height) imageSize)
     {
-        using var image = ImageGenerator.Next(width, height);
+        using var image = ImageGenerator.Next(imageSize.width, imageSize.height);
         var filter = Filters.PrewittX;
         RunSingle(image, filter);
     }
 
     [Theory]
     [MemberData("ImageSizes")]
-    public void Apply_PrewittY(int width, int height)
+    public void Apply_PrewittY((int width, int height) imageSize)
     {
-        using var image = ImageGenerator.Next(width, height);
+        using var image = ImageGenerator.Next(imageSize.width, imageSize.height);
         var filter = Filters.PrewittY;
         RunSingle(image, filter);
     }
 
     [Theory]
     [MemberData("ImageSizes")]
-    public void Apply_RandomFilter(int width, int height)
+    public void Apply_RandomFilter((int width, int height) imageSize)
     {
-        using var image = ImageGenerator.Next(width, height);
+        using var image = ImageGenerator.Next(imageSize.width, imageSize.height);
         var filter = FilterGenerator.Next(size: 5);
         RunSingle(image, filter);
     }
@@ -98,17 +98,17 @@ public abstract class CompareUnsafeParallel
 [Trait("Suite", "Coverage")]
 public class CompareUnsafeParallel_Coverage : CompareUnsafeParallel
 {
-    public static TheoryData<int, int> ImageSizes => new() { { 50, 50 } };
+    public static TheoryData<(int width, int height)> ImageSizes => new() { (width: 50, height: 50) };
 }
 
 [Trait("Suite", "All")]
 public class CompareUnsafeParallel_All : CompareUnsafeParallel
 {
-    public static TheoryData<int, int> ImageSizes => new()
+    public static TheoryData<(int width, int height)> ImageSizes => new()
     {
-        { 1, 1 },
-        { 32, 32 },
-        { 256, 144 },
-        { 640, 360 },
+        (width: 1, height: 1),
+        (width: 32, height: 32),
+        (width: 256, height: 144),
+        (width: 640, height: 360),
     };
 }
